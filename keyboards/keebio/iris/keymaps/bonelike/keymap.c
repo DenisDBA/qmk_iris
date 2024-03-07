a485405cab30e52cc16ce4906d450b341d492bd2
Denis <142374163+DenisDBA@users.noreply.github.com>
2024-03-07 07:09:24 +0100

    Add function key layer (#5)
    * remove hotkey layer
    
    * add new layer
    
    * spacing and clean vp unused keys

--- keyboards/keebio/iris/keymaps/bonelike/keymap.c
+++ keyboards/keebio/iris/keymaps/bonelike/keymap.c
@@ -11,32 +11,22 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
-#define _HOTKEY 4
+#define _FUNC 4
 
-// function declaration
-void winrun(char prog[]);
 
 enum custom_keycodes {
   BONE = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
-  HOTKEY,
-  MEDGE,
-  MOUTLOOK,
-  MNOTEPAD,
-  MTEAMS,
-  MFIREFOX,
-  MVSCODE,
-  MSQL,
-  MONENOTE,
+  FUNC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BONE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                           KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,                           KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_J,    KC_D,    DE_UDIA, DE_ADIA, DE_ODIA,                             KC_P,    KC_H,    KC_L,    KC_M,    KC_W,    DE_SS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -44,19 +34,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT,  KC_F,    KC_V,   KC_U,     KC_A,   KC_X,    KC_HOME,          KC_END,  DE_Y,    DE_Z,   KC_COMM,  KC_DOT,  KC_K,    KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                           LT(_HOTKEY,KC_ESC),LT(_LOWER,KC_SPC), LT(_HOTKEY,KC_BSPC),         KC_SPC,  LT(_RAISE,KC_ENT),   LT(_ADJUST,KC_TAB)
+                           LT(_FUNC,KC_ESC),LT(_LOWER,KC_SPC), KC_BSPC,         KC_SPC,  LT(_RAISE,KC_ENT),   LT(_ADJUST,KC_TAB)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                           KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+     KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,                           KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO,   KC_NO,   KC_BSPC, KC_DEL,   KC_NO,    KC_NO,                             KC_0,   KC_7,    KC_8,     KC_9,   KC_PLUS, KC_MINS,
+     KC_NO,   _______, KC_BSPC, KC_DEL,  _______, _______,                             KC_0,   KC_7,    KC_8,     KC_9,   KC_PLUS, KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL, KC_TRNS, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                                KC_RBRC, RSFT_T(KC_4), RCTL_T(KC_5), LALT_T(KC_6), LGUI_T(KC_COMM), KC_DOT, 
+     KC_DEL, _______, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                                _______, RSFT_T(KC_4), RCTL_T(KC_5), LALT_T(KC_6), LGUI_T(KC_COMM), KC_DOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, KC_TRNS, KC_LBRC, KC_INS, KC_DOWN, KC_PGDN, KC_LPRN,           KC_SPC, KC_RCBR, KC_1, KC_2, KC_3, KC_COLN, KC_TRNS,
+     BL_STEP, _______, KC_HOME, KC_END, KC_DOWN, KC_PGDN, _______,           KC_SPC, _______, KC_1, KC_2, KC_3, KC_COLN, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     CW_TOGG, _______, KC_DEL,                    KC_0,    ADJUST, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -66,11 +56,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, DE_EURO, DE_UNDS, DE_LBRC, DE_RBRC, DE_CIRC,                            DE_EXLM, DE_LABK, DE_RABK, DE_EQL,  DE_AMPR, KC_TRNS, 
+     RGB_TOG, DE_EURO, DE_UNDS, DE_LBRC, DE_RBRC, DE_CIRC,                            DE_EXLM, DE_LABK, DE_RABK, DE_EQL,  DE_AMPR, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_ASTR,                            DE_QUES, DE_LPRN, DE_RPRN, DE_MINS, DE_COLN, DE_AT, 
+     RGB_MOD, DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_ASTR,                            DE_QUES, DE_LPRN, DE_RPRN, DE_MINS, DE_COLN, DE_AT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, DE_HASH, DE_DLR, DE_PIPE, DE_TILD, KC_GRV, KC_LPRN,          KC_TRNS, DE_PLUS, DE_PERC, DE_DQUO, DE_QUOT, DE_SCLN, KC_TRNS, 
+     KC_MUTE, DE_HASH,  DE_DLR, DE_PIPE, DE_TILD,  KC_GRV, _______,          _______, DE_PLUS, DE_PERC, DE_DQUO, DE_QUOT, DE_SCLN, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, ADJUST, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -89,28 +79,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, _______, _______,                   _______, DF(_BONE), DF(_BONE)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
-    [_HOTKEY] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_SLEP, LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),               LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),LGUI(KC_0),KC_BSPC,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_J,    KC_D,    KC_U,    KC_A,    KC_X,                               MNOTEPAD,    KC_H,  KC_L,  LGUI(KC_2), KC_W,   DE_SS,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC, MVSCODE, MTEAMS, LCTL_T(KC_I), MEDGE, MOUTLOOK,                          KC_B,    MONENOTE, KC_R,    MSQL,    KC_G,    KC_Q,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, MFIREFOX, KC_V,   DE_UDIA, DE_ADIA, DE_ODIA, KC_HOME,          KC_END,  DE_Y,    DE_Z,   KC_COMM,  KC_DOT,  KC_K,  KC_RSFT,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______,  _______,   _______,                 _______,  _______,   _______ 
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+
+  [_FUNC] = LAYOUT(
+ //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+ //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, _______, KC_BSPC, KC_DEL,  _______, _______,                            _______,   KC_F7,  KC_F8,   KC_F9,  KC_F12,  KC_MINS,
+ //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, _______, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                               _______, RSFT_T(KC_F4), RCTL_T(KC_F5), LALT_T(KC_F6), LGUI_T(KC_F11), KC_DOT,
+ //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, _______, KC_HOME, KC_END, KC_DOWN, KC_PGDN, _______,           _______, _______, KC_F1,   KC_F2,   KC_F3,    KC_F10, _______,
+ //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                  _______,  ADJUST,  _______
+                               // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
-
-
-void winrun(char prog[]) {
-  SEND_STRING(SS_LALT(" "));
-  SEND_STRING(SS_DELAY(200) "<");
-  send_string(prog);
-  SEND_STRING(SS_DELAY(100) "\n");
-}
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -149,55 +132,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-   // Macros
-   case MEDGE:
-      if (record->event.pressed) {
-        winrun("edge");
-      }
-      return false;
-      break;
-   case MOUTLOOK:
-      if (record->event.pressed) {
-        winrun("outlook");
-      }
-      return false;
-      break;
-   case MNOTEPAD:
-      if (record->event.pressed) {
-        winrun("notepad++");
-      }
-      return false;
-      break;
-   case MTEAMS:
-      if (record->event.pressed) {
-        winrun("teams.exe");
-      }
-      return false;
-      break;
-   case MFIREFOX:
-      if (record->event.pressed) {
-        winrun("firefox");
-      }
-      return false;
-      break;
-   case MVSCODE:
-      if (record->event.pressed) {
-        winrun("visual studio");
-      }
-      return false;
-      break;
-   case MSQL:
-      if (record->event.pressed) {
-        winrun("sql");
-      }
-      return false;
-      break;
-   case MONENOTE:
-      if (record->event.pressed) {
-        winrun("onenote.exe");
-      }
-      return false;
-      break;
   }
   return true;
 }
@@ -219,7 +153,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_DEL:
         case KC_UNDS:
         case DE_UNDS:
-        case DE_MINS: 
+        case DE_MINS:
             return true;
 
         default:
