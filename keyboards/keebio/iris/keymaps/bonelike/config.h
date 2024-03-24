de3918cd4c8b6eaa6388ff16f6486b286868b79a
Denis <142374163+DenisDBA@users.noreply.github.com>
2024-03-24 07:30:13 +0100

    get fixes from master (#7)
    * New mouse layer (#6)
    
    * change layer key + layout
    
    * change mouse layer to defaut
    
    * lower mouse max speed
    
    * add trailing new line
    
    * move 0 to main thumbkey
    
    * enable encoder
    
    * remove unused keys and move dot on number layer
    
    * add encoder logik
    
    * add combos esc and insert

--- keyboards/keebio/iris/keymaps/bonelike/config.h
+++ keyboards/keebio/iris/keymaps/bonelike/config.h
@@ -1,1 +1,4 @@
-#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#   define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Mouse key settings
+#   define MOUSEKEY_MAX_SPEED   7       // reduce Mouse speed default is 10
--- keyboards/keebio/iris/keymaps/bonelike/keymap.c
+++ keyboards/keebio/iris/keymaps/bonelike/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT,  KC_F,    KC_V,   KC_U,     KC_A,   KC_X,    KC_HOME,          KC_END,  DE_Y,    DE_Z,   KC_COMM,  KC_DOT,  KC_K,    KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                           LT(_FUNC,KC_ESC),LT(_LOWER,KC_SPC), KC_BSPC,         KC_SPC,  LT(_RAISE,KC_ENT),   LT(_ADJUST,KC_TAB)
+                           LT(_FUNC,KC_ESC),LT(_LOWER,KC_SPC), LT(_ADJUST,KC_BSPC),         KC_SPC,  LT(_RAISE,KC_ENT),   LT(_ADJUST,KC_TAB)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -42,13 +42,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,                           KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO,   _______, KC_BSPC, KC_DEL,  _______, _______,                             KC_0,   KC_7,    KC_8,     KC_9,   KC_PLUS, KC_MINS,
+     KC_NO,   _______, KC_BSPC, KC_DEL,  _______, _______,                             KC_0,   KC_7,    KC_8,     KC_9,   KC_PLUS, KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_DEL, _______, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                                _______, RSFT_T(KC_4), RCTL_T(KC_5), LALT_T(KC_6), LGUI_T(KC_COMM), KC_DOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, KC_HOME, KC_END, KC_DOWN, KC_PGDN, _______,           KC_SPC, _______, KC_1, KC_2, KC_3, KC_COLN, _______,
+     BL_STEP, _______, KC_HOME, KC_END, KC_DOWN, KC_PGDN, _______,           KC_SPC, _______, KC_1, KC_2, KC_3, KC_DOT, KC_NO,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    CW_TOGG, _______, KC_DEL,                    KC_0,    ADJUST, KC_P0
+                                    CW_TOGG, _______, KC_DEL,                    KC_0,    KC_0, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, LGUI_T(KC_C), LALT_T(KC_T), LCTL_T(KC_PSTE), LSFT_T(KC_COPY), KC_VOLU,   KC_WH_U, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, KC_CUT,  KC_VOLD, _______,       DF(_ADJUST), KC_WH_D, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______,
+     _______, _______, KC_ACL2, KC_ACL1, KC_ACL0,  KC_VOLD, DF(_BONE),       DF(_ADJUST), KC_WH_D, _______, _______, _______, KC_BTN4, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, DF(_BONE), DF(_BONE)
+                                    _______, _______, _______,                   KC_BTN1, KC_BTN2, KC_BTN3
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, _______, KC_BSPC, KC_DEL,  _______, _______,                            _______,   KC_F7,  KC_F8,   KC_F9,  KC_F12,  KC_MINS,
+    _______, _______, KC_BSPC, KC_DEL,  _______, _______,                            _______,   KC_F7,  KC_F8,   KC_F9,  KC_F12,  KC_NO,
  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, _______, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                               _______, RSFT_T(KC_F4), RCTL_T(KC_F5), LALT_T(KC_F6), LGUI_T(KC_F11), KC_DOT,
+    _______, _______, KC_LEFT, KC_RGHT, KC_UP, KC_PGUP,                               _______, RSFT_T(KC_F4), RCTL_T(KC_F5), LALT_T(KC_F6), LGUI_T(KC_F11), KC_NO,
  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
     _______, _______, KC_HOME, KC_END, KC_DOWN, KC_PGDN, _______,           _______, _______, KC_F1,   KC_F2,   KC_F3,    KC_F10, _______,
  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -163,7 +163,73 @@ bool caps_word_press_user(uint16_t keycode) {
 
 // Combos
 const uint16_t PROGMEM insert_combo[] = {KC_F, KC_V, COMBO_END};
+const uint16_t PROGMEM insert_combo_onefinger[] = {LGUI_T(KC_C), KC_J, COMBO_END};
+const uint16_t PROGMEM esc_combo[] = {LSFT_T(KC_E), KC_O, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(insert_combo, KC_INSERT),
+    COMBO(insert_combo_onefinger, KC_INSERT),
+    COMBO(esc_combo, KC_ESC),
 };
+
+// Encoder
+bool encoder_update_user(uint8_t index, bool clockwise) {
+// left encoder
+    if (index == 0) {
+        switch(biton32(layer_state)){
+             case 2:
+                if (clockwise) {
+                register_code(KC_LCTL);
+                tap_code(KC_3);
+                unregister_code(KC_LCTL);
+                } else {
+                register_code(KC_LCTL);
+                tap_code(KC_1);
+                unregister_code(KC_LCTL);
+                }
+                break;
+            case 1:
+                if (clockwise) {
+                tap_code(KC_MINS);
+                } else {
+                tap_code(KC_EQL);
+
+                }
+                break;
+            default:
+                if (clockwise){
+                    tap_code(KC_VOLU);
+                } else{
+                    tap_code(KC_VOLD);
+                }
+                break;
+      }
+    }
+// right encoder
+    else if (index == 2) {
+        switch(biton32(layer_state)){
+            case 1:
+                if (clockwise){
+                    tap_code(KC_WH_R);
+                } else{
+                    tap_code(KC_WH_L);
+                }
+                break;
+            case _ADJUST:
+                if (clockwise){
+                    tap_code(KC_VOLU);
+                } else{
+                    tap_code(KC_VOLD);
+                }
+                break;
+            default:
+                if (clockwise) {
+                    tap_code(KC_WH_U);
+                } else {
+                    tap_code(KC_WH_D);
+                }
+                break;
+      }
+}
+    return false;
+}
