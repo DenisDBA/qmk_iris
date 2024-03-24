5fff2957e4d7aa08809b36cd8fe21a400cfa968c
Denis Axlers <denis@axlers.de>
2024-03-24 07:25:54 +0100

    enable encoder to merge with master

--- keyboards/keebio/iris/keymaps/bonelike/rules.mk
+++ keyboards/keebio/iris/keymaps/bonelike/rules.mk
@@ -2,7 +2,7 @@
 #   change yes to no to disable
 #
 MOUSEKEY_ENABLE = yes        # Mouse keys
-ENCODER_ENABLE = no
+ENCODER_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
